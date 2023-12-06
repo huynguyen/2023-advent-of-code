50b933fbc4308ea49af4a8acffdbe38af0be54ab
Huy Nguyen <weehuy@gmail.com>
2023-12-06 00:22:58 -0800

    Tried the overlapping ranges but had some problems.
    * See comments about gaps.
    * Even then this was not actually faster.

--- day-05/src/bin/part2.rs
+++ day-05/src/bin/part2.rs
@@ -1,3 +1,4 @@
+
 use itertools::Itertools;
 
 fn main() {
@@ -28,9 +29,22 @@ impl Mapping {
             .find(|r| r.dest_contains(&id))
             .map_or(id, |r| r.source_for(&id))
     }
+
+    fn overlap(&self, range: Ranges) -> Option<Mapping> {
+        self.ranges
+            .iter()
+            .find(|r| r.overlap(&range).is_some())
+            .map(|r| {
+                let new_range = r.overlap(&range).unwrap();
+                Mapping {
+                    name: format!("{}..={}", new_range.dest_start, new_range.dest_end()),
+                    ranges: vec![new_range],
+                }
+            })
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Ranges {
     source_start: u64,
     dest_start: u64,
@@ -57,6 +71,38 @@ impl Ranges {
     fn source_for(&self, dest_id: &u64) -> u64 {
         let offset = *dest_id - self.dest_start;
         self.source_start + offset
+    }
+
+    fn dest_end(&self) -> u64 {
+        self.dest_start + self.range_len
+    }
+
+    fn source_end(&self) -> u64 {
+        self.source_start + self.range_len
+    }
+
+    fn overlap(&self, range: &Ranges) -> Option<Ranges> {
+        // |--- self ---|
+        //           |--- range ---|
+        if range.dest_start <= self.source_end() && self.source_start <= range.dest_end() {
+            let new_len = self.source_end() - range.dest_start;
+            Some(Ranges {
+                source_start: 0,
+                dest_start: range.source_start,
+                range_len: new_len,
+            })
+        } else if range.dest_end() >= self.source_start && self.source_end() >= range.source_start {
+            let new_len = range.dest_end() - self.source_start;
+            Some(Ranges {
+                source_start: 0,
+                dest_start: self.source_start,
+                range_len: new_len,
+            })
+            //            |--- self ---|
+            //  |--- range ---|
+        } else {
+            Some(*range)
+        }
     }
 }
 
@@ -86,7 +132,7 @@ fn part2(input: &str) -> String {
                     .tuples::<(&u64, &u64)>()
                     .map(|(&start, &len)| start..(start + len))
                     // .flat_map(|r| r.map(|i| i).collect::<Vec<u64>>() )
-                    .collect()
+                    .collect(),
             )
         })
         .expect("List of seeds.");
@@ -94,13 +140,50 @@ fn part2(input: &str) -> String {
     // Process maps
     let lookups: Vec<_> = section_iter.map(gen_map).collect();
 
-    (0..u64::MAX).find(|&id| {
-        let possible_seed = lookups.iter().rev().fold(id, |mut acc, map| {
-            acc = map.dest_to_source(acc);
-            acc
-        });
-        seeds.iter().any(|r| r.contains(&possible_seed))
-    }).unwrap().to_string()
+    let humidity_to_location = lookups.last().expect("humidity-to-location must be here.");
+    let lowest_range = humidity_to_location
+        .ranges
+        .iter()
+        // Need to generate ranges for the gaps since if there is no mapping it is a direct
+        // translation. In the example the answer is the following range but there is no mapping
+        // override for it. In input1.txt the soil dest range is contiguous but that isn't
+        // guarnteed. Maybe a simplification of all this is just look at highest soil map dest and
+        // go from 0 to that.
+        // .chain(once(&Ranges { source_start: 0, dest_start: 0, range_len: 51}))
+        .sorted_by_key(|r| r.dest_start)
+        .find(|&r| {
+            dbg!(r);
+            lookups
+                .iter()
+                .rev()
+                .skip(1)
+                .fold(
+                    Some(Mapping {
+                        name: "initial location range".into(),
+                        ranges: vec![*r],
+                    }),
+                    |acc, map| {
+                        acc.and_then(|a| match map.overlap(*a.ranges.iter().nth(0).unwrap()) {
+                            Some(m) => Some(m),
+                            _ => None,
+                        })
+                    },
+                )
+                .is_some()
+        }).and_then(|r| (r.dest_start..=r.dest_end()).into());
+
+
+    lowest_range
+        .unwrap()
+        .find(|&id| {
+            let possible_seed = lookups.iter().rev().fold(id, |mut acc, map| {
+                acc = map.dest_to_source(acc);
+                acc
+            });
+            seeds.iter().any(|r| r.contains(&possible_seed))
+        })
+        .unwrap()
+        .to_string()
 }
 
 fn get_list_of_nums(nums: &str) -> Vec<u64> {
