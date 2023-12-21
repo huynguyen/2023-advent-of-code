2451d7336376648680b1a7630f1d224d748b3609
Huy Nguyen <weehuy@gmail.com>
2023-12-20 18:45:42 -0800

    Rewrite part2 to use area of polygon using shoelace formula and pick's theorem.

--- day-18/src/bin/part2.rs
+++ day-18/src/bin/part2.rs
@@ -1,6 +1,6 @@
-use std::iter::successors;
+use core::iter;
 
-use itertools::{repeat_n, Itertools};
+use itertools::Itertools;
 
 #[cfg(feature = "dhat-heap")]
 #[global_allocator]
@@ -15,150 +15,72 @@ fn main() {
     dbg!(output);
 }
 
-fn print_map(input: &[Vec<char>]) {
-    for r in input {
-        let just = r.iter().join(" ");
-        println!("{:?}", just);
-    }
-}
-
-fn build_map(trenches: &[Trench]) -> Vec<Vec<char>> {
-    let maxx = trenches.iter().map(|t| t.x).max().unwrap_or(0);
-    let minx = trenches.iter().map(|t| t.x).min().unwrap();
-    let maxy = trenches.iter().map(|t| t.y).max().unwrap_or(0);
-    let miny = trenches.iter().map(|t| t.y).min().unwrap();
-
-    dbg!(minx, maxx, miny, maxy);
-    let dx = maxx + minx.abs();
-    let dy = maxy + miny.abs();
-    let row = repeat_n('.', (dx + 1) as usize).collect_vec();
-    let mut map = (0..dy + 1)
-        .map(|_| row.clone())
-        .collect_vec();
-
-    for t in trenches {
-        // dbg!(t);
-        let x = (t.x + minx.abs()) as usize;
-        let y = (t.y + miny.abs()) as usize;
-        map[y][x] = match t.dir {
-            Direction::Up => '^',
-            Direction::Down => 'v',
-            Direction::Left => '<',
-            Direction::Right => '>',
-            Direction::Start => 's',
-        };
-    }
-
-    for t in trenches {
-        let x = (t.x + minx.abs()) as usize;
-        let y = (t.y + miny.abs()) as usize;
-        let belowcorner = map.get(y + 1).and_then(|r| r.get(x));
-        let abovecorner = y
-            .checked_sub(1)
-            .and_then(|yidx| map.get(yidx))
-            .and_then(|r| r.get(x));
-        let rightcorner = map.get(y).and_then(|r| r.get(x + 1));
-        let leftcorner = map
-            .get(y)
-            .and_then(|r| x.checked_sub(1).and_then(|idx| r.get(idx)));
-
-        match (belowcorner, rightcorner, leftcorner, abovecorner) {
-            (Some(bch), Some(rch), _, _) if ![bch, rch].contains(&&'.') => {
-                map[y][x] = 'F';
-            }
-            (Some(bch), _, Some(lch), _) if ![bch, lch].contains(&&'.') => {
-                map[y][x] = '7';
-            }
-            (_, _, Some(lch), Some(ach)) if ![ach, lch].contains(&&'.') => {
-                map[y][x] = 'J';
-            }
-            (_, Some(rch), _, Some(ach)) if ![ach, rch].contains(&&'.') => {
-                map[y][x] = 'L';
-            }
-            _ => {}
-        }
-    }
-
-    map
-}
-
-fn fill_inner(map: &mut Vec<Vec<char>>) {
-    for row in map {
-        let new_row = row
-            .iter()
-            .enumerate()
-            .map(|(idx, c)| {
-                match c {
-                    '.' => {
-                        // .filter(|&ch| ['S', 'F', '7', '|'].contains(ch))
-                        let allow = ['^', 'v', 's', 'F', '7'];
-                        let crosses = row[idx + 1..]
-                            .iter()
-                            .dedup()
-                            .filter(|&ch| allow.contains(ch))
-                            .count();
-                        if crosses % 2 == 1 {
-                            '#'
-                        } else {
-                            *c
-                        }
-                    }
-                    _ => *c,
-                }
-            })
-            .collect_vec();
-        *row = new_row;
-    }
-}
-
 fn part2(input: &str) -> String {
-    let start = Trench {
+    let start = Vertex {
         x: 0,
         y: 0,
         color: "#000000".into(),
         dir: Direction::Start,
     };
-    let trenches = input
+    let vertices = input
         .lines()
         .map(Instruction::from)
         .fold(vec![start], |mut acc, inst| {
             match acc.last() {
                 Some(prev) => {
-                    let instructions = successors(Some(prev.clone()), |p: &Trench| {
-                        Some(p.from_instruction(&inst))
-                    })
-                    .skip(1)
-                    .take(inst.steps as usize);
-                    acc.extend(instructions);
+                    acc.push(prev.from_instruction(&inst));
                 }
                 _ => {}
             }
             acc
         });
 
-    let mut map = build_map(&trenches);
-    todo!();
-    fill_inner(&mut map);
+    let perimeter = vertices
+            .iter()
+            .tuple_windows()
+            .map(|(v1, v2)| calculate_distance(&v1, &v2))
+            .sum::<i64>();
 
-    print_map(&map);
-    map.iter().map(|r| r.iter().filter(|ch| *ch != &'.').count()).sum::<usize>().to_string()
+    let area = vertices
+        .iter()
+        .tuple_windows()
+        .flat_map(|(v1, v2)| {
+            let (p, s) = calculate_area(&v1, &v2);
+            iter::once(p).chain(iter::once(s))
+        })
+        .collect_vec();
+
+
+
+    let result = ((area.iter().sum::<i64>() + perimeter)/ 2) + 1;
+    result.to_string()
+}
+
+fn calculate_area(v1: &Vertex, v2: &Vertex) -> (i64, i64) {
+    let ad = v1.x * v2.y;
+    let bd = v2.x * v1.y;
+    (ad, -bd)
+}
+
+fn calculate_distance(v1: &Vertex, v2: &Vertex) -> i64 {
+    (v2.x - v1.x).abs() + (v2.y - v1.y).abs()
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
-struct Trench {
+struct Vertex {
     x: i64,
     y: i64,
     color: String,
     dir: Direction,
 }
 
-impl Trench {
+impl Vertex {
     fn from_instruction(&self, instruction: &Instruction) -> Self {
-        let (x, y) = match &instruction.dir {
-            Direction::Up => (self.x, self.y - 1),
-            Direction::Down => (self.x, self.y + 1),
-            Direction::Left => (self.x - 1, self.y),
-            Direction::Right => (self.x + 1, self.y),
+        let (x, y) = match (&instruction.dir, instruction.steps as i64) {
+            (Direction::Up, n) => (self.x, self.y - n),
+            (Direction::Down, n) => (self.x, self.y + n),
+            (Direction::Left, n) => (self.x - n, self.y),
+            (Direction::Right, n) => (self.x + n, self.y),
             _ => panic!("Should not get a start direction"),
         };
 
@@ -187,6 +109,29 @@ struct Instruction {
     color: String,
 }
 
+impl Instruction {
+    #[allow(dead_code)]
+    fn old_from(value: &str) -> Self {
+        let mut it = value.split_whitespace();
+        let dir = it
+            .next()
+            .and_then(|d| Some(Direction::from(d)))
+            .expect("A valid direction.");
+        let steps = it
+            .next()
+            .and_then(|n| n.parse::<u32>().ok())
+            .expect("A valid number of steps.");
+        let color = it
+            .next()
+            .and_then(|s| s.strip_prefix('('))
+            .and_then(|s| s.strip_suffix(')'))
+            .expect("hexcolor in expected format.")
+            .to_string();
+
+        Self { dir, steps, color }
+    }
+}
+
 impl From<&str> for Instruction {
     fn from(value: &str) -> Self {
         let mut it = value.split_whitespace().skip(2);
@@ -198,7 +143,9 @@ impl From<&str> for Instruction {
             .expect("hexcolor in expected format.")
             .to_string();
 
-        let dir = color.pop().and_then(|d| d.to_digit(10))
+        let dir = color
+            .pop()
+            .and_then(|d| d.to_digit(10))
             .map(Direction::from)
             .expect("A valid direction.");
 
@@ -208,6 +155,7 @@ impl From<&str> for Instruction {
         Self { dir, steps, color }
     }
 }
+
 impl From<u32> for Direction {
     fn from(value: u32) -> Self {
         match value {
@@ -233,7 +181,93 @@ impl From<&str> for Direction {
 
 #[cfg(test)]
 mod test {
+    use std::iter;
+
     use super::*;
+    #[test]
+    fn shoelace_area() {
+        let result = [(1, 6), (3, 1), (7, 2), (4, 4), (8, 5)]
+            .iter()
+            .chain(iter::once(&(1, 6)))
+            .map(|p| Vertex {
+                x: p.0,
+                y: p.1,
+                color: "#000000".into(),
+                dir: Direction::Start,
+            })
+            .tuple_windows()
+            .flat_map(|(v1, v2)| {
+                let (p, s) = calculate_area(&v1, &v2);
+                iter::once(p).chain(iter::once(s))
+            })
+            .collect_vec();
+
+        let result = result.iter().sum::<i64>() / 2;
+
+        assert_eq!(16, result);
+    }
+
+    #[test]
+    fn part1_area() {
+        let sample = "R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)";
+
+        let start = Vertex {
+            x: 0,
+            y: 0,
+            color: "#000000".into(),
+            dir: Direction::Start,
+        };
+        let vertices =
+            sample
+                .lines()
+                .map(Instruction::old_from)
+                .fold(vec![start.clone()], |mut acc, inst| {
+                    match acc.last() {
+                        Some(prev) => {
+                            acc.push(prev.from_instruction(&inst));
+                        }
+                        _ => {}
+                    }
+                    acc
+                });
+
+        // dbg!(&vertices);
+
+        let perimeter = vertices
+            .iter()
+            .tuple_windows()
+            .map(|(v1, v2)| calculate_distance(&v1, &v2))
+            .sum::<i64>();
+
+        let area = vertices
+            .iter()
+            .tuple_windows()
+            .flat_map(|(v1, v2)| {
+                let (p, s) = calculate_area(&v1, &v2);
+                iter::once(p).chain(iter::once(s))
+            })
+            .collect_vec();
+
+        // dbg!(&area);
+        let result = area.iter().sum::<i64>() + perimeter;
+        // dbg!(result, result / 2);
+        // assert_eq!(38, perimeter);
+        assert_eq!(62, (result / 2)+1);
+    }
+
     #[test]
     fn instructions() {
         let sample = "R 6 (#70c710)";
